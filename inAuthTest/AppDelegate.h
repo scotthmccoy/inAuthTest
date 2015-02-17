db779b50ee9bc0760a7d26c9c639b2a8f0f5f12d
Scott McCoy <scotthmccoy@gmail.com>
2015-02-17 12:33:50 -0800

    First Commit

---
+++ inAuthTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  inAuthTest
+//
+//  Created by Scott McCoy on 2/17/15.
+//  Copyright (c) 2015 ScottSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ inAuthTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  inAuthTest
+//
+//  Created by Scott McCoy on 2/17/15.
+//  Copyright (c) 2015 ScottSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
